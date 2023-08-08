c48c1e29fc7b3f065e491f0b64ce3f1942b72ede
Carmen <carmen@MacBook-Air-de-Carmen.local>
2023-08-08 11:39:58 -0600

    save changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,10 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
+    //ktor
+    implementation("io.ktor:ktor-server-core:2.3.3")
+    implementation("io.ktor:ktor-server-netty:2.3.3")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
